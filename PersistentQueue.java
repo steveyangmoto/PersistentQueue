3e5b17cfb608e1b0a4b8e7b4d1c329d92bd868e2
steveyangmoto <steveyangmoto@gmail.com>
2017-01-22 11:38:28 -0500

    Update PersistentQueue.java
    Made it generic

--- PersistentQueue.java
+++ PersistentQueue.java
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import android.content.Context;
+import android.util.Log;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
@@ -27,72 +29,69 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by SteveYang on 3/11/15.
- * a crash-resistent, and globally accessible queue for Android SDK, that holds String objects
+ * a crash-resistent, and globally accessible queue for Android SDK, that holds any serializable objects
  */
-public class PersistentQueue {
-    private static final String FILE_NAME = "PersistentQueue";
-    private static PersistentQueue persistentQueue = null;
-    private ArrayList<String> myQueue = null;
-    
-    private PersistentQueue() {
-        myQueue = loadQueueData(FILE_NAME);
+public class PersistentQueue<T extends Serializable> {
+    private static final String TAG = "PersistentQueue";
+    private ArrayList<T> myQueue = null;
+    private Context context;
+    private String fileName;
+
+    public PersistentQueue(Context context,String fileName) {
+        this.context = context;
+        this.fileName = fileName;
+        myQueue = loadQueueData(fileName);
         if (myQueue == null) {
-            myQueue = new ArrayList<String>();
+            myQueue = new ArrayList<T>();
         }
     }
-    
-    public static PersistentQueue getInstance() {
-        if (persistentQueue == null) {
-            persistentQueue = new PersistentQueue();
-        }
-        return persistentQueue;
-    }
-    
+
     /**
      * Adds an element to the head of the queue
      *
      * @param obj
      */
-    public void enqueue(String obj) {
+    public void enqueue(T obj) {
         synchronized (this) {
             myQueue.add(obj);
-            persistQueueData(myQueue, FILE_NAME);
+            persistQueueData(myQueue, fileName);
         }
     }
-    
+
     /**
      * Retrieves and removes the head of this queue, or returns null if this queue is empty.
      *
      * @return the head of this queue, or null if this queue is empty
      */
-    public String dequeue() {
+    public T dequeue() {
         synchronized (this) {
             if (myQueue.size() > 0) {
-                String element = myQueue.get(0);
+                T element = myQueue.get(0);
                 myQueue.remove(0);
-                persistQueueData(myQueue, FILE_NAME);
+                persistQueueData(myQueue, fileName);
                 return element;
             }
         }
         return null;
     }
-    
+
     /**
      * Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
      *
      * @return the head of this queue, or null if this queue is empty
      */
-    public String peek() {
+    public T peek() {
         if (myQueue.size() > 0) {
             return myQueue.get(0);
         }
         return null;
     }
-    
+
     /**
      * Retrieves, but does not remove, the element of this queue at the specified index,
      * or returns null if either this queue is empty or the index is out of bounds.
@@ -100,13 +99,13 @@ public class PersistentQueue {
      * @param index
      * @return the element of this queue at the specified index, or null if this queue is empty
      */
-    public String peekAt(int index) {
+    public T peekAt(int index) {
         if (index >= 0 && index < myQueue.size()) {
             return myQueue.get(index);
         }
         return null;
     }
-    
+
     /**
      * Inserts the specified object into the queue at the specified location.
      * The object is inserted before any previous element at the specified location.
@@ -117,18 +116,18 @@ public class PersistentQueue {
      * @return true if the index to insert at in the queue is valid,
      * false if the index is negative or greater than the size of the queue
      */
-    public boolean insertAt(int index, String obj) {
+    public boolean insertAt(int index, T obj) {
         synchronized (this) {
             if (index >= 0 && index <= myQueue.size()) {
                 myQueue.add(index, obj);
-                persistQueueData(myQueue, FILE_NAME);
+                persistQueueData(myQueue, fileName);
                 return true;
-                
+
             }
         }
         return false;
     }
-    
+
     /**
      * Removes the object at the specified location
      *
@@ -140,13 +139,13 @@ public class PersistentQueue {
         synchronized (this) {
             if (index >= 0 && index < myQueue.size()) {
                 myQueue.remove(index);
-                persistQueueData(myQueue, FILE_NAME);
+                persistQueueData(myQueue, fileName);
                 return true;
             }
         }
         return false;
     }
-    
+
     /**
      * Retrieves the size of the queue
      *
@@ -155,27 +154,51 @@ public class PersistentQueue {
     public int getSize() {
         return myQueue.size();
     }
-    
-    private void persistQueueData(ArrayList<String> queue, String filePath) {
+
+    private void persistQueueData(ArrayList<T> queue, String filePath) {
         ObjectOutputStream oos = null;
+        FileOutputStream fileOutputStream = null;
         try {
-            oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filePath)));
+            fileOutputStream = context.openFileOutput(filePath, Context.MODE_PRIVATE);
+            oos = new ObjectOutputStream(new BufferedOutputStream(fileOutputStream));
             oos.writeObject(queue);
-            oos.close();
         } catch (IOException ioe) {
-            return;
+            Log.e(TAG, "exception: " + ioe.getMessage());
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (Exception ignore) {
+            }
         }
     }
-    
-    private ArrayList<String> loadQueueData(String filePath) {
+
+    private ArrayList<T> loadQueueData(String filePath) {
         ObjectInputStream ois = null;
+        FileInputStream fileInputStream = null;
         try {
-            ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(filePath)));
-            ArrayList<String> queue = (ArrayList<String>) ois.readObject();
+            fileInputStream = context.openFileInput(filePath);
+            ois = new ObjectInputStream(new BufferedInputStream(fileInputStream));
+            ArrayList<T> queue = (ArrayList<T>) ois.readObject();
             ois.close();
             return queue;
         } catch (Exception ex) {
+            Log.e(TAG, "exception: " + ex.getMessage());
             return null;
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (Exception ignore) {
+            }
         }
     }
 }
